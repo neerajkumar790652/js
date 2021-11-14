9105c6e016bf292fb310b705bc8ad6bb048266c6
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2021-11-14 19:03:11 +0530

    Add files via upload

---
+++ Q&A/index.js
@@ -0,0 +1,40 @@
+function Task1(){
+    var a= document.getElementById('q1');
+    a.innerText="yes";
+
+}
+function Task2(){
+    var a= document.getElementById('q2');
+    a.innerText="yes";
+
+}
+function Task3(){
+    var a= document.getElementById('q3');
+    a.innerText="yes";
+
+}
+function Task4(){
+    var a= document.getElementById('q4');
+    a.innerText="yes";
+
+}
+function Task5(){
+    var a= document.getElementById('q5');
+    a.innerText="28";
+
+}
+function Task6(){
+    var a= document.getElementById('q6');
+    a.innerText="11";
+
+}
+function Task7(){
+    var a= document.getElementById('q7');
+    a.innerText="Geekster";
+
+}
+function Task8(){
+    var a= document.getElementById('q8');
+    a.innerText="Kabir sir";
+
+}
